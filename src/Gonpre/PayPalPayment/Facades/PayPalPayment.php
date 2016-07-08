9225d8f3c9f095e1713a914831209b4ec0e5f2f8
Francisco Gonzalez <gonpre@gmail.com>
2016-07-08 11:52:27 -0700

    Added Billing plans and Agreements

--- src/Gonpre/PayPalPayment/Facades/PayPalPayment.php
+++ src/Gonpre/PayPalPayment/Facades/PayPalPayment.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Facade;
 class PayPalPayment extends Facade
 {
 
+    const PAYMENT_DEFINITION_TYPE_REGULAR = \Gonpre\PayPalPayment\PayPalPayment::PAYMENT_DEFINITION_TYPE_REGULAR;
+    const PAYMENT_DEFINITION_TYPE_TRIAL   = \Gonpre\PayPalPayment\PayPalPayment::PAYMENT_DEFINITION_TYPE_TRIAL;
+    const PLAN_TYPE_FIXED                 = \Gonpre\PayPalPayment\PayPalPayment::PLAN_TYPE_FIXED;
+    const PLAN_TYPE_INFINITE              = \Gonpre\PayPalPayment\PayPalPayment::PLAN_TYPE_INFINITE;
+
     /**
      * Get the registered name of the component.
      *
---
+++ src/Gonpre/PayPalPayment/Plan.php
@@ -0,0 +1,121 @@
+<?php namespace Gonpre\PayPalPayment;
+
+use PayPal\Api\Patch;
+use PayPal\Api\PatchRequest;
+use PayPal\Api\Plan;
+use PayPal\Common\PayPalModel;
+use PayPal\Rest\ApiContext;
+use PayPal\Transport\PayPalRestCall;
+
+trait Plan
+{
+    /**
+     * Return a new plan
+     *
+     * @param  array|null $data
+     * @return \PayPal\Api\Plan
+     */
+    public function plan($data = null)
+    {
+        return new Plan($data);
+    }
+
+    /**
+     * Get the plan using the paypal id
+     *
+     * @param  string                           $planId
+     * @param  \PayPal\Rest\ApiContext|null     $apiContext
+     * @param  \PayPal\Transport\PayPalRestCall $restCall
+     * @return \PayPal\Api\Plan
+     */
+    public function getPlan($planId, ApiContext $apiContext = null, PayPalRestCall $restCall = null)
+    {
+        return Plan::get($planId, $apiContext, $restCall);
+    }
+
+    /**
+     * Activate a current plan
+     *
+     * @param  sintrg                  $planId
+     * @param  \PayPal\Rest\ApiContext $apiContext
+     * @return \PayPal\Api\Plan
+     */
+    public function activatePlan($planId, ApiContext $apiContext)
+    {
+        $plan  = $this->getPlan($planId, $apiContext);
+        $patch = new Patch();
+        $value = new PayPalModel(['state' => 'ACTIVE']);
+
+        $patch->setOp('replace')
+            ->setPath('/')
+            ->setValue($value);
+
+        $patchRequest = new PatchRequest();
+        $patchRequest->addPatch($patch);
+
+        $plan->update($patchRequest, $apiContext);
+
+        return $plan;
+    }
+
+    /**
+     * Update a current plan
+     *
+     * @param  sintrg                  $planId
+     * @param  array                   $data This should be an array with the indexes 'payment_definitions' or 'merchant_preferences'
+     * @param  \PayPal\Rest\ApiContext $apiContext
+     * @return \PayPal\Api\Plan
+     */
+    public function updatePlan($id, array $data, ApiContext $apiContext)
+    {
+        $plan  = $this->getPlan($planId, $apiContext);
+
+        if (!empty($data['payment_definitions'])) {
+            $paymentDefinitions = $plan->getPaymentDefinitions();
+            $paymentDefinitionId = $paymentDefinitions[0]->getId();
+
+            $patch = new Patch();
+            $patch->setOp('replace')
+                ->setPath('/payment-definitions/' . $paymentDefinitionId)
+                ->setValue($data['payment_definitions']);
+
+            $patchRequest = new PatchRequest();
+            $patchRequest->addPatch($patch);
+
+            $plan->update($patchRequest, $apiContext);
+        }
+
+        if (!empty($data['merchant_preferences'])) {
+            $merchantPreferences = $plan->getMerchantPreferences();
+            $merchantPreferencesId = $merchantPreferences[0]->getId();
+
+            $patch = new Patch();
+            $patch->setOp('replace')
+                ->setPath('/merchant-preferences/' . $merchantPreferencesId)
+                ->setValue($data['merchant_preferences']);
+
+            $patchRequest = new PatchRequest();
+            $patchRequest->addPatch($patch);
+
+            $plan->update($patchRequest, $apiContext);
+        }
+
+        $plan  = $this->getPlan($planId, $apiContext);
+
+        return $plan;
+    }
+
+    /**
+     * Delete a plan by id
+     *
+     * @param  string                  $id
+     * @param  \PayPal\Rest\ApiContext $apiContext
+     * @return \PayPal\Api\Plan
+     */
+    public function deletePlan($id, ApiContext $apiContext)
+    {
+        $plan = $this->getPlan($id, $apiContext);
+
+        return $plan->delete($spiContext);
+    }
+}
